577892a84d41637574dd43029f4813a44f7e4f7a
James Boobier <139986710+JamesBoobierMOJ@users.noreply.github.com>
2023-08-24 15:58:40 +0100

    Fix linting issue

--- server/routes/prisoner-overview/index.ts
+++ server/routes/prisoner-overview/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const prisonerOverviewRouter = Router().get('/', async (req, res, next) => {
   // TODO - move code into here from router index
 })
