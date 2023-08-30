c99851a48758db4aa3ed576c4a6230e9e9b64bee
James Boobier <139986710+JamesBoobierMOJ@users.noreply.github.com>
2023-08-30 15:06:12 +0100

    Update

--- server/routes/staffDashboard/staffDashboardController.ts
+++ server/routes/staffDashboard/staffDashboardController.ts
@@ -1,5 +1,4 @@
 import { RequestHandler } from 'express'
-import type { HTTPError } from 'superagent'
 import PrisonService from '../../services/prisonService'
 import logger from '../../../logger'
 import StaffDashboardView from './staffDashboardView'
@@ -10,17 +9,12 @@ export default class StaffDashboardController {
   constructor(private readonly prisonService: PrisonService) {}
 
   getView: RequestHandler = async (req, res, next): Promise<void> => {
-    const { token } = req.user
-
-    const errors: ErrorMessage[] = []
-
-    const { prisonSelected } = req.query
-    let prisonSelectList = null
-    let prisonersList = null
-
     try {
-      const prisons = await this.prisonService.getListOfPrisons(token)
-      prisonSelectList = prisons.map((prison: Prison) => {
+      const { prisonSelected } = req.query
+
+      const prisons = await this.prisonService.getListOfPrisons(req.user.token)
+
+      const prisonSelectList = prisons.map((prison: Prison) => {
         return {
           text: prison.name,
           value: prison.id,
@@ -34,17 +28,17 @@ export default class StaffDashboardController {
         selected: false,
       })
 
+      const errors: ErrorMessage[] = []
+
       if (prisonSelected === 'unset') {
         errors.push({
           text: 'You must select a prison',
           href: '#',
         })
       }
-    } catch (err) {
-      // next(err)
-    }
+      const token = res.locals?.user?.token
 
-    if (prisonSelected && prisonSelected !== 'unset') {
+      let prisonersList = null
       try {
         // TODO add dynamic pagination and sorting
         prisonersList = await this.prisonService.getListOfPrisoners(
@@ -55,15 +49,18 @@ export default class StaffDashboardController {
           'releaseDate',
           'DESC',
         )
-      } catch (e) {
-        errors.push({
-          text: 'Unexpected error when getting prisoners - please try again later or contact support if issues persist',
-          href: '#',
-        })
+      } catch (error) {
+        logger.error('Error fetching prisoner list: ', error)
       }
-    }
 
-    const view = new StaffDashboardView(prisonSelectList, <string>prisonSelected, prisonersList, errors)
-    res.render('pages/staff-dashboard', { ...view.renderArgs })
+      const view = new StaffDashboardView(prisonSelectList, <string>prisonSelected, prisonersList, errors)
+      res.render('pages/staff-dashboard', { ...view.renderArgs })
+    } catch (error) {
+      const errors = [
+        { text: 'Unexpected error - please try again later or contact support if issues persist', href: '#' },
+      ]
+      logger.error(error, errors)
+      res.render('pages/staff-dashboard', { errors })
+    }
   }
 }
