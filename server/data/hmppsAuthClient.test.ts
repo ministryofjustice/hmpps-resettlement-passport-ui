04f8964cda9970c273ef57e0d41ada3497d5bc90
James Boobier <139986710+JamesBoobierMOJ@users.noreply.github.com>
2023-09-06 12:43:32 +0100

    RP2-478 Prepare preprod environment and tidy up

--- server/data/hmppsAuthClient.test.ts
+++ server/data/hmppsAuthClient.test.ts
@@ -50,47 +50,4 @@ describe('hmppsAuthClient', () => {
       expect(output).toEqual(['role1', 'role2'])
     })
   })
-
-  describe('getSystemClientToken', () => {
-    it('should instantiate the redis client', async () => {
-      tokenStore.getToken.mockResolvedValue(token.access_token)
-      await hmppsAuthClient.getSystemClientToken(username)
-    })
-
-    it('should return token from redis if one exists', async () => {
-      tokenStore.getToken.mockResolvedValue(token.access_token)
-      const output = await hmppsAuthClient.getSystemClientToken(username)
-      expect(output).toEqual(token.access_token)
-    })
-
-    it('should return token from HMPPS Auth with username', async () => {
-      tokenStore.getToken.mockResolvedValue(null)
-
-      fakeHmppsAuthApi
-        .post(`/oauth/token`, 'grant_type=client_credentials&username=Bob')
-        .basicAuth({ user: config.apis.hmppsAuth.systemClientId, pass: config.apis.hmppsAuth.systemClientSecret })
-        .matchHeader('Content-Type', 'application/x-www-form-urlencoded')
-        .reply(200, token)
-
-      const output = await hmppsAuthClient.getSystemClientToken(username)
-
-      expect(output).toEqual(token.access_token)
-      expect(tokenStore.setToken).toBeCalledWith('Bob', token.access_token, 240)
-    })
-
-    it('should return token from HMPPS Auth without username', async () => {
-      tokenStore.getToken.mockResolvedValue(null)
-
-      fakeHmppsAuthApi
-        .post(`/oauth/token`, 'grant_type=client_credentials')
-        .basicAuth({ user: config.apis.hmppsAuth.systemClientId, pass: config.apis.hmppsAuth.systemClientSecret })
-        .matchHeader('Content-Type', 'application/x-www-form-urlencoded')
-        .reply(200, token)
-
-      const output = await hmppsAuthClient.getSystemClientToken()
-
-      expect(output).toEqual(token.access_token)
-      expect(tokenStore.setToken).toBeCalledWith('%ANONYMOUS%', token.access_token, 240)
-    })
-  })
 })
