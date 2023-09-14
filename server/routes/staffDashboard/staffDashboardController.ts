26b986077cc8c040f9599c1b728592c6e7ee7ab7
kivi corn <akiva.moshecorn@digital.justice.gov.uk>
2023-09-14 13:44:06 +0100

    FEAT: search by prisoner name or id

--- server/routes/staffDashboard/staffDashboardController.ts
+++ server/routes/staffDashboard/staffDashboardController.ts
@@ -9,6 +9,7 @@ export default class StaffDashboardController {
   getView: RequestHandler = async (req, res, next): Promise<void> => {
     const { token } = req.user
     const { userActiveCaseLoad } = res.locals
+    const { searchInput = '' } = req.query as { searchInput: string }
 
     const errors: ErrorMessage[] = []
     let prisonersList = null
@@ -22,8 +23,9 @@ export default class StaffDashboardController {
         200,
         'releaseDate',
         'DESC',
+        <string>searchInput,
       )
-      const view = new StaffDashboardView(prisonersList, errors)
+      const view = new StaffDashboardView(prisonersList, errors, searchInput)
       res.render('pages/staff-dashboard', { ...view.renderArgs })
     } catch (err) {
       next(err)
