2725644491eb3a354317391576e5dcaf3e1ee6ab
vickybirtlesmoj <132053630+vickybirtlesmoj@users.noreply.github.com>
2023-09-27 13:55:06 +0100

    Future date validation

--- server/routes/finance-id/confirm-assessment.ts
+++ server/routes/finance-id/confirm-assessment.ts
@@ -7,6 +7,7 @@ type ErrorMessage = {
   dateAssessmentDay: null | string
   dateAssessmentMonth: null | string
   dateAssessmentYear: null | string
+  futureDate: null | string
 }
 const confirmAssessmentRouter = express.Router().get('/', async (req: Request, res, next) => {
   const { prisonerData } = req
@@ -17,18 +18,34 @@ const confirmAssessmentRouter = express.Router().get('/', async (req: Request, r
     dateAssessmentDay: null,
     dateAssessmentMonth: null,
     dateAssessmentYear: null,
+    futureDate: null,
   }
 
-  const {
-    assessmentDate,
-    isIdRequired,
-    isBankAccountRequired,
-    dateAssessmentDay,
-    dateAssessmentMonth,
-    dateAssessmentYear,
-  } = params
-  console.log(assessmentDate)
-  if (!isIdRequired || !isBankAccountRequired || !dateAssessmentDay || !dateAssessmentMonth || !dateAssessmentYear) {
+  const { isIdRequired, isBankAccountRequired, dateAssessmentDay, dateAssessmentMonth, dateAssessmentYear } = params
+
+  function isDateInFuture(inputDay: string, inputMonth: string, inputYear: string) {
+    // Create a Date object using the input values
+    const currentDate = new Date()
+    const inputDate = new Date(Number(inputYear), Number(inputMonth) - 1, Number(inputDay)) // Month is zero-based
+
+    // Check if the input date is in the future
+    return inputDate > currentDate
+  }
+
+  const isFutureDate = isDateInFuture(
+    <string>dateAssessmentDay,
+    <string>dateAssessmentMonth,
+    <string>dateAssessmentYear,
+  )
+
+  if (
+    !isIdRequired ||
+    !isBankAccountRequired ||
+    !dateAssessmentDay ||
+    !dateAssessmentMonth ||
+    !dateAssessmentYear ||
+    isFutureDate
+  ) {
     const message = 'Select an option'
     const dateFieldMissingMessage = 'The date of assessment must include a '
     errorMsg = {
@@ -37,6 +54,7 @@ const confirmAssessmentRouter = express.Router().get('/', async (req: Request, r
       dateAssessmentDay: dateAssessmentDay ? null : `${dateFieldMissingMessage} day`,
       dateAssessmentMonth: dateAssessmentMonth ? null : `${dateFieldMissingMessage} month`,
       dateAssessmentYear: dateAssessmentYear ? null : `${dateFieldMissingMessage} year`,
+      futureDate: isFutureDate ? 'The date of assessment must be in the past' : null,
     }
     res.render('pages/assessment', { prisonerData, params, req, errorMsg })
     return
