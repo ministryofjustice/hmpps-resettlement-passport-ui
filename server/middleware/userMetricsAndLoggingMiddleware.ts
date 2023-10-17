4e2782a5a5d6830d17bc4ffcb21847f9315a074d
James Boobier <139986710+JamesBoobierMOJ@users.noreply.github.com>
2023-10-17 16:17:37 +0100

    RP2-681 PoC for user metrics counter

---
+++ server/middleware/userMetricsAndLoggingMiddleware.ts
@@ -0,0 +1,19 @@
+import { RequestHandler } from 'express'
+import logger from '../../logger'
+import userMetricsCounter from '../monitoring/customMetrics'
+
+const userMetricsAndLoggingMiddleware = (): RequestHandler => {
+  return async (req, res, next) => {
+    logger.info(
+      `User: ${req.user.username} Session: ${req.sessionID} Auth Source: ${req.user.authSource} requested page ${req.url}`,
+    )
+    userMetricsCounter.inc({
+      path: req.path.toLowerCase(),
+      auth_type: res.locals.user.authSource,
+      ...(res.locals.userActiveCaseLoad && { caseload: res.locals.userActiveCaseLoad.caseLoadId }),
+    })
+    next()
+  }
+}
+
+export default userMetricsAndLoggingMiddleware
