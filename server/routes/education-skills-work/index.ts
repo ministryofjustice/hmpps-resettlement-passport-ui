3c88442e633e392eeeb23f1433f681f1d44d9491
vickybirtlesmoj <132053630+vickybirtlesmoj@users.noreply.github.com>
2023-10-25 17:20:53 +0100

    Initial commit.

--- server/routes/education-skills-work/index.ts
+++ server/routes/education-skills-work/index.ts
@@ -1,8 +1,27 @@
 import express from 'express'
+import { RPClient } from '../../data'
+import logger from '../../../logger'
 
 const educationSkillsWorkRouter = express.Router().get('/', async (req, res, next) => {
   const { prisonerData } = req
-  res.render('pages/education-skills-work', { prisonerData })
+  const rpClient = new RPClient(req.user.token, req.sessionID, req.user.username)
+  let educationSkillsWork: { error?: boolean } = {}
+
+  try {
+    educationSkillsWork = await rpClient.get(
+      `/resettlement-passport/prisoner/${prisonerData.personalDetails.prisonerNumber}/work-readiness`,
+    )
+  } catch (err) {
+    logger.warn(
+      `Session: ${req.sessionID} Cannot retrieve education, skills, and work info for ${prisonerData.personalDetails.prisonerNumber} ${err.status} ${err}`,
+    )
+    educationSkillsWork.error = true
+  }
+
+  res.render('pages/education-skills-work', {
+    educationSkillsWork,
+    prisonerData,
+  })
 })
 
 export default educationSkillsWorkRouter
