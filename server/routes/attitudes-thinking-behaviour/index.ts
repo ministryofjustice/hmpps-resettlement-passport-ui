eb53cd3ddb5a698c9b6cb55990a6ccfda99f9aa9
James Boobier <139986710+JamesBoobierMOJ@users.noreply.github.com>
2023-10-27 09:32:48 +0100

    RP2-706 Fix compilation error (#134)

--- server/routes/attitudes-thinking-behaviour/index.ts
+++ server/routes/attitudes-thinking-behaviour/index.ts
@@ -3,7 +3,7 @@ import { Services } from '../../services'
 import AttitudesThinkingBehaviourController from './attitudesThinkingBehaviourController'
 
 export default (router: Router, services: Services) => {
-  const attitudesThinkingBehaviourController = new AttitudesThinkingBehaviourController(services.prisonService)
+  const attitudesThinkingBehaviourController = new AttitudesThinkingBehaviourController(services.rpService)
 
   router.get('/attitudes-thinking-and-behaviour', [attitudesThinkingBehaviourController.getView])
 }
