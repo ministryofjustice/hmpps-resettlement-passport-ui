8c4431e3e3da5444a40818c702d2a074a585486e
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2023-11-27 11:13:04 +0000

    RP2-731 Improvements to feature flags mechanism (#163)
    Co-authored-by: James Boobier <139986710+JamesBoobierMOJ@users.noreply.github.com>

--- localstack/setup-s3.sh
+++ localstack/setup-s3.sh
@@ -9,4 +9,4 @@ export AWS_ENDPOINT_URL=http://localhost:4566
 aws s3api create-bucket --region eu-west-2 --bucket hmpps-resettlement-passport-ui-config --create-bucket-configuration LocationConstraint=eu-west-2
 aws s3api list-buckets
 
-aws s3 cp /etc/localstack/init/ready.d/local.json s3://hmpps-resettlement-passport-ui-config/feature-flags/local.json
+aws s3 cp /etc/localstack/init/ready.d/flags.json s3://hmpps-resettlement-passport-ui-config/feature-flags/flags.json
