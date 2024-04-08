04e5565fcaae03911121fde2b1693d4dc4af3b09
James Boobier <139986710+JamesBoobierMOJ@users.noreply.github.com>
2024-04-08 16:20:40 +0100

    update

--- server/middleware/setUpStaticResources.ts
+++ server/middleware/setUpStaticResources.ts
@@ -25,8 +25,8 @@ export default function setUpStaticResources(): Router {
     router.use('/assets', express.static(path.join(process.cwd(), dir), cacheControl))
   })
 
-  Array.of('/node_modules/govuk_frontend_toolkit/images').forEach(dir => {
-    router.use('/assets/images/icons', express.static(path.join(process.cwd(), dir), cacheControl))
+  Array.of('/node_modules/jquery/dist/jquery.min.js').forEach(dir => {
+    router.use('/assets/js/jquery.min.js', express.static(path.join(process.cwd(), dir), cacheControl))
   })
 
   // Don't cache dynamic resources
