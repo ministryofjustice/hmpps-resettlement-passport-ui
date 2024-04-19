a15be646aefa6545122e1a323777e4246dd43a00
kivi corn <akiva.moshecorn@digital.justice.gov.uk>
2024-04-19 09:59:14 +0100

    education skills work

--- server/routes/education-skills-work/educationSkillsWorkView.ts
+++ server/routes/education-skills-work/educationSkillsWorkView.ts
@@ -3,6 +3,8 @@ import { PrisonerData } from '../../@types/express'
 import { CrsReferralResponse } from '../../data/model/crsReferralResponse'
 import { EducationSkillsWorkResponse } from '../../data/model/educationSkillsWorkResponse'
 import { AssessmentsInformation } from '../../data/model/assessmentInformation'
+import { CaseNotesHistory } from '../../data/model/caseNotesHistory'
+import { CaseNotesCreators } from '../../data/model/caseNotesCreators'
 
 export default class EducationSkillsWorkView implements View {
   constructor(
@@ -10,6 +12,13 @@ export default class EducationSkillsWorkView implements View {
     private readonly crsReferrals: CrsReferralResponse,
     private readonly educationSkillsWork: EducationSkillsWorkResponse,
     private readonly assessmentData: AssessmentsInformation,
+    private readonly caseNotesData: CaseNotesHistory,
+    private readonly caseNotesCreators: CaseNotesCreators,
+    private readonly createdByUserId: string,
+    private readonly pageSize: string,
+    private readonly page: string,
+    private readonly sort: string,
+    private readonly days: string,
     private readonly errors: ErrorMessage[] = [],
   ) {
     // no op
@@ -20,6 +29,13 @@ export default class EducationSkillsWorkView implements View {
     crsReferrals: CrsReferralResponse
     educationSkillsWork: EducationSkillsWorkResponse
     assessmentData: AssessmentsInformation
+    caseNotesData: CaseNotesHistory
+    caseNotesCreators: CaseNotesCreators
+    createdByUserId: string
+    pageSize: string
+    page: string
+    sort: string
+    days: string
     errors: ErrorMessage[]
   } {
     return {
@@ -27,6 +43,13 @@ export default class EducationSkillsWorkView implements View {
       crsReferrals: this.crsReferrals,
       educationSkillsWork: this.educationSkillsWork,
       assessmentData: this.assessmentData,
+      caseNotesData: this.caseNotesData,
+      caseNotesCreators: this.caseNotesCreators,
+      createdByUserId: this.createdByUserId,
+      pageSize: this.pageSize,
+      page: this.page,
+      sort: this.sort,
+      days: this.days,
       errors: this.errors.length !== 0 ? this.errors : null,
     }
   }
