a5131543c5c40217bb521a84fdd0177e9c57525a
kivi corn <akiva.moshecorn@digital.justice.gov.uk>
2024-04-19 10:40:58 +0100

    health

--- server/routes/health-status/healthStatusController.ts
+++ server/routes/health-status/healthStatusController.ts
@@ -11,6 +11,14 @@ export default class HealthStatusController {
     try {
       const { prisonerData } = req
       const { token } = req.user
+      const {
+        page = '0',
+        pageSize = '10',
+        sort = 'occurenceDateTime%2CDESC',
+        days = '0',
+        createdByUserId = '0',
+      } = req.query
+
       const crsReferrals = await this.rpService.getCrsReferrals(
         token,
         req.sessionID,
@@ -25,7 +33,37 @@ export default class HealthStatusController {
         'HEALTH',
       )
 
-      const view = new HealthStatusView(prisonerData, crsReferrals, assessmentData)
+      const caseNotesData = await this.rpService.getCaseNotesHistory(
+        token,
+        req.sessionID,
+        prisonerData.personalDetails.prisonerNumber as string,
+        'HEALTH',
+        createdByUserId as string,
+        pageSize as string,
+        page as string,
+        sort as string,
+        days as string,
+      )
+
+      const caseNotesCreators = await this.rpService.getCaseNotesCreators(
+        token,
+        req.sessionID,
+        prisonerData.personalDetails.prisonerNumber as string,
+        'HEALTH',
+      )
+
+      const view = new HealthStatusView(
+        prisonerData,
+        crsReferrals,
+        assessmentData,
+        caseNotesData,
+        caseNotesCreators,
+        createdByUserId as string,
+        pageSize as string,
+        page as string,
+        sort as string,
+        days as string,
+      )
       res.render('pages/health', { ...view.renderArgs })
     } catch (err) {
       next(err)
