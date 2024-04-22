8203c157f62f4892c6769dfef98c607b84b21d9e
James Boobier <139986710+JamesBoobierMOJ@users.noreply.github.com>
2024-04-22 09:54:29 +0100

    PSFR-1189 Remove call to deprecated auth endpoint

--- server/data/hmppsAuthClient.ts
+++ server/data/hmppsAuthClient.ts
@@ -1,16 +1,6 @@
 import type TokenStore from './tokenStore'
-import logger from '../../logger'
 import config from '../config'
 import RestClient from './restClient'
-
-export interface User {
-  name: string
-  activeCaseLoadId: string
-}
-
-export interface UserRole {
-  roleCode: string
-}
 
 export default class HmppsAuthClient {
   constructor(private readonly tokenStore: TokenStore) {
@@ -19,16 +9,5 @@ export default class HmppsAuthClient {
 
   private static restClient(token: string): RestClient {
     return new RestClient('HMPPS Auth Client', config.apis.hmppsAuth, token)
-  }
-
-  getUser(token: string): Promise<User> {
-    logger.info(`Getting user details: calling HMPPS Auth`)
-    return HmppsAuthClient.restClient(token).get({ path: '/api/user/me' }) as Promise<User>
-  }
-
-  getUserRoles(token: string): Promise<string[]> {
-    return HmppsAuthClient.restClient(token)
-      .get({ path: '/api/user/me/roles' })
-      .then(roles => (<UserRole[]>roles).map(role => role.roleCode))
   }
 }
