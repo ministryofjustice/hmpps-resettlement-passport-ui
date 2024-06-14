6bcf005f45efefd57bdb9487b44ce4fa3efdfaf9
kivi corn <akiva.moshecorn@digital.justice.gov.uk>
2024-06-14 17:11:16 +0100

    remove reporting FE

--- server/utils/constants.ts
+++ server/utils/constants.ts
@@ -151,7 +151,6 @@ export const RISK_ASSESSMENT_ENUMS_DICTIONARY: { [key: string]: { name: string; 
 }
 
 export const FEATURE_FLAGS: { [key: string]: string } = {
-  REPORTING: 'reporting',
   ADD_APPOINTMENTS: 'addAppointments',
   DELIUS_CASE_NOTES: 'nDeliusCaseNotes',
   RESETTLEMENT_ASSESSMENT: 'resettlementAssessment',
