f53a8f8176bd64e2c8bd6e68f74b76dbbf49f002
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2024-06-21 15:08:14 +0100

    update

--- server/utils/analytics.ts
+++ server/utils/analytics.ts
@@ -1,36 +1,18 @@
 import { TelemetryClient } from 'applicationinsights'
 
 // eslint-disable-next-line no-shadow
-export enum PyfEvent {}
-// TODO
-// TIMEOUT_EVENT = 'PYF_SessionTimeout',
-// REGISTRATION_ERROR_EVENT = 'PYF_FirstTimeRegistrationError',
+export enum PsfrEvent {
+  STATUS_UPDATE_EVENT = 'PSFR_StatusUpdate',
+}
 
 export type KeyValue = {
   key: string
   value: string
 }
 
-export const trackEvent = (appInsightsClient: TelemetryClient, name: string, properties: KeyValue[]) => {
+export const trackEvent = (appInsightsClient: TelemetryClient, name: string, properties: object) => {
   if (name && appInsightsClient) {
     appInsightsClient.trackEvent({ name, properties })
     appInsightsClient.flush()
   }
-}
-
-function mapErrorsToAnalytics(errors: Array<Express.ValidationError>): KeyValue[] {
-  return errors.map(item => {
-    return {
-      key: 'errorField',
-      value: item.href,
-    }
-  })
-}
-
-export const userProperties = (userId: string) => {
-  return [{ key: 'userId', value: userId }]
-}
-
-export const errorProperties = (errors: Array<Express.ValidationError>, userId: string) => {
-  return [...mapErrorsToAnalytics(errors), ...userProperties(userId)]
 }
