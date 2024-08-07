3431b6a5c3dffe10bf8d0882868ccd99a339eb29
kivi corn <akiva.moshecorn@digital.justice.gov.uk>
2024-08-07 17:27:03 +0100

    add cypress test

--- integration_tests/mockApis/redis.ts
+++ integration_tests/mockApis/redis.ts
@@ -11,3 +11,78 @@ export async function resetRedisCache() {
 
   await redisClient.disconnect()
 }
+
+export async function initRedisCache(pairs: { key: string; value: string }[]) {
+  const redisClient: RedisClient = createRedisClient()
+  await redisClient.connect()
+
+  const redisSetPromises = pairs.map(async pair => {
+    await redisClient.set(pair.key, pair.value)
+  })
+
+  await Promise.all(redisSetPromises)
+
+  await redisClient.disconnect()
+}
+
+export async function initRedisCacheForNullExistingAssessment() {
+  const currentPage = {
+    id: 'MEET_HEALTHCARE_TEAM',
+    title: null,
+    questionsAndAnswers: [
+      {
+        question: {
+          id: 'MEET_HEALTHCARE_TEAM',
+          title: 'Does the person in prison want to meet with a prison healthcare team?',
+          subTitle: null,
+          type: 'RADIO',
+          options: [
+            { id: 'YES', displayText: 'Yes', description: null, exclusive: false, nestedQuestions: null },
+            { id: 'NO', displayText: 'No', description: null, exclusive: false, nestedQuestions: null },
+            {
+              id: 'NO_ANSWER',
+              displayText: 'No answer provided',
+              description: null,
+              exclusive: false,
+              nestedQuestions: null,
+            },
+          ],
+          validationType: 'MANDATORY',
+        },
+        answer: null,
+        originalPageId: 'MEET_HEALTHCARE_TEAM',
+      },
+    ],
+  }
+
+  const assessment = {
+    questionsAndAnswers: [
+      {
+        question: 'REGISTERED_WITH_GP',
+        questionTitle: 'Is the person in prison registered with a GP surgery outside of prison?',
+        questionType: 'RADIO',
+        pageId: 'REGISTERED_WITH_GP',
+        answer: { answer: 'YES', displayText: 'Yes', '@class': 'StringAnswer' },
+      },
+    ],
+  }
+
+  const answered = ['REGISTERED_WITH_GP']
+
+  initRedisCache([
+    {
+      key: 'currentPage:USER1:A8731DY:HEALTH',
+      value: JSON.stringify(currentPage),
+    },
+    {
+      key: 'assessment:USER1:A8731DY:HEALTH',
+      value: JSON.stringify(assessment),
+    },
+    {
+      key: 'answered:USER1:A8731DY:HEALTH',
+      value: JSON.stringify(answered),
+    },
+  ])
+
+  return null
+}
