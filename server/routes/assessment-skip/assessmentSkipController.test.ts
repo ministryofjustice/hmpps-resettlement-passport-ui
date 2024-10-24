0f6affb590f409b2137232035e14e2ec173b7055
abdulrkalam <abdul.rahuman@digital.justice.gov.uk>
2024-10-24 15:05:24 +0100

    PSFR-1792   Add controller unit tests for Assessment skip routes

--- server/routes/assessment-skip/assessmentSkipController.test.ts
+++ server/routes/assessment-skip/assessmentSkipController.test.ts
@@ -1,4 +1,77 @@
+import request from 'supertest'
+import type { Express } from 'express'
 import { validateAssessmentSkipForm } from './assessmentSkipController'
+import RpService from '../../services/rpService'
+import Config from '../../s3Config'
+import { stubPrisonerDetails } from '../testutils/testUtils'
+import { configHelper } from '../configHelperTest'
+import { appWithAllRoutes } from '../testutils/appSetup'
+
+let app: Express
+let rpService: jest.Mocked<RpService>
+const config: jest.Mocked<Config> = new Config() as jest.Mocked<Config>
+
+beforeEach(() => {
+  rpService = new RpService() as jest.Mocked<RpService>
+  jest.mock('applicationinsights', () => jest.fn())
+  configHelper(config)
+
+  app = appWithAllRoutes({
+    services: {
+      rpService,
+    },
+  })
+  stubPrisonerDetails(rpService)
+})
+afterEach(() => {
+  jest.resetAllMocks()
+})
+describe('getView', () => {
+  it('Happy path with default query params', async () => {
+    await request(app)
+      .get('/assessment-skip?prisonerNumber=123')
+      .expect(200)
+      .expect(res => expect(res.text).toMatchSnapshot)
+  })
+})
+
+describe('submitForm', () => {
+  it('Happy path with default inputs', async () => {
+    const postAssessmentSkipSpy = jest.spyOn(rpService, 'postAssessmentSkip').mockImplementation()
+    const service = {
+      whySkipChoice: 'EARLY_RELEASE',
+      supportingInfo: 'Some Info',
+      prisonerNumber: '123',
+    }
+    await request(app)
+      .post('/assessment-skip?prisonerNumber=123')
+      .send(service)
+      .expect(302)
+      .expect(res =>
+        expect(res.headers.location).toEqual('/assessment-task-list?prisonerNumber=123&type=RESETTLEMENT_PLAN'),
+      )
+
+    expect(postAssessmentSkipSpy).toHaveBeenCalledWith('123', {
+      reason: 'EARLY_RELEASE',
+      moreInfo: 'Some Info',
+    })
+  })
+})
+
+describe('submitForm validation', () => {
+  it('Validates  input', async () => {
+    const postAssessmentSkipSpy = jest.spyOn(rpService, 'postAssessmentSkip').mockImplementation()
+    const service = {
+      supportingInfo: 'Some Info',
+      prisonerNumber: '123',
+    }
+    await request(app)
+      .post('/assessment-skip?prisonerNumber=123')
+      .send(service)
+      .expect(302)
+      .expect(res => expect(res.headers.location).toContain('validationErrors'))
+  })
+})
 
 describe('validateAssessmentSkipForm', () => {
   it.each(['COMPLETED_IN_OASYS', 'COMPLETED_IN_ANOTHER_PRISON', 'EARLY_RELEASE', 'TRANSFER', 'OTHER'])(
@@ -7,22 +80,22 @@ describe('validateAssessmentSkipForm', () => {
       expect(validateAssessmentSkipForm({ whySkipChoice: choice })).toBeNull()
     },
   )
+})
 
-  it('gives an error for missing whySkipChoice', () => {
-    expect(validateAssessmentSkipForm({ somethingElse: 'earlyRelease' })).toEqual({
-      whySkipChoice: 'This field is required',
-    })
+it('gives an error for missing whySkipChoice', () => {
+  expect(validateAssessmentSkipForm({ somethingElse: 'earlyRelease' })).toEqual({
+    whySkipChoice: 'This field is required',
   })
+})
 
-  it('gives an error for unexpected whySkipChoice', () => {
-    expect(validateAssessmentSkipForm({ whySkipChoice: 'potato' })).toEqual({
-      whySkipChoice: 'This field is required',
-    })
+it('gives an error for unexpected whySkipChoice', () => {
+  expect(validateAssessmentSkipForm({ whySkipChoice: 'potato' })).toEqual({
+    whySkipChoice: 'This field is required',
   })
+})
 
-  it('gives an error for empty', () => {
-    expect(validateAssessmentSkipForm({})).toEqual({
-      whySkipChoice: 'This field is required',
-    })
+it('gives an error for empty', () => {
+  expect(validateAssessmentSkipForm({})).toEqual({
+    whySkipChoice: 'This field is required',
   })
 })
