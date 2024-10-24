f61ebef42aafa0994c363341dfe74bdf699ee059
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2024-10-24 09:56:19 +0100

    PSFR-1820 Remove unused legacy assessment code (#421)
    * PSFR-1820 Remove unused legacy assessment code
    
    * update

--- server/routes/add-bank-account/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-
-const addBankAccountRouter = express.Router().get('/', (req, res, next) => {
-  const { prisonerData } = req
-  res.render('pages/add-bank-account', { prisonerData })
-})
-
-export default addBankAccountRouter
