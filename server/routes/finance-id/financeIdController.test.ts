59fd22156db922ef0a5a65eaeb0971412498fde7
JY0280 <jean-yves@hippodigital.co.uk>
2024-10-28 15:08:13 +0000

    Added further scenarios around validation, error and confirm add an id

--- server/routes/finance-id/financeIdController.test.ts
+++ server/routes/finance-id/financeIdController.test.ts
@@ -34,10 +34,72 @@ describe('getAddAnIdView', () => {
       .expect(200)
       .expect(res => expect(res.text).toMatchSnapshot())
   })
-  it('Submit empty ID application form', async () => {
+  it('Validation error - submit empty ID application form', async () => {
     await request(app)
       .get(
         '/finance-and-id/add-an-id-further/?idType=&applicationSubmittedDay=&applicationSubmittedMonth=&applicationSubmittedYear=&prisonerNumber=A8731DY&isPriorityApplication=&costOfApplication=&driversLicenceApplicationMadeAt=&driversLicenceType=&courtDetails=&caseNumber=&isUkNationalBornOverseas=&countryBornIn=&haveGro=&existingIdTypes=',
+      )
+      .expect(200)
+      .expect(res => expect(res.text).toMatchSnapshot())
+  })
+  it('Validation error - submit ID application via birth certificate without a date', async () => {
+    await request(app)
+      .get(
+        '/finance-and-id/add-an-id-further/?idType=Birth+certificate&applicationSubmittedDay=&applicationSubmittedMonth=&applicationSubmittedYear=&prisonerNumber=A8731DY&isPriorityApplication=&costOfApplication=&driversLicenceApplicationMadeAt=&driversLicenceType=&courtDetails=&caseNumber=&isUkNationalBornOverseas=&countryBornIn=&haveGro=&existingIdTypes=',
+      )
+      .expect(200)
+      .expect(res => expect(res.text).toMatchSnapshot())
+  })
+  it('Validation error - submit submit ID application via birth certificate with invalid date', async () => {
+    await request(app)
+      .get(
+        '/finance-and-id/add-an-id-further/?idType=Birth+certificate&applicationSubmittedDay=13&applicationSubmittedMonth=13&applicationSubmittedYear=2000&prisonerNumber=A8731DY&isPriorityApplication=&costOfApplication=&driversLicenceApplicationMadeAt=&driversLicenceType=&courtDetails=&caseNumber=&isUkNationalBornOverseas=&countryBornIn=&haveGro=&existingIdTypes=',
+      )
+      .expect(200)
+      .expect(res => expect(res.text).toMatchSnapshot())
+  })
+  it('Error case - submit ID application without type or date', async () => {
+    await request(app)
+      .get('/finance-and-id/add-an-id-further/?prisonerNumber=A8731DY')
+      .expect(500)
+      .expect(res => expect(res.text).toMatchSnapshot())
+  })
+  it('Confirm add an ID - birth certificate submit without cost or answers', async () => {
+    await request(app)
+      .get(
+        '/finance-and-id/confirm-add-an-id/?countryBornIn=&costOfApplication=&applicationSubmittedYear=2000&applicationSubmittedMonth=12&applicationSubmittedDay=12&idType=Birth+certificate&prisonerNumber=A8731DY&existingIdTypes=',
+      )
+      .expect(200)
+      .expect(res => expect(res.text).toMatchSnapshot())
+  })
+  it('Confirm add an ID - birth certificate submit with cost below zero and no answers', async () => {
+    await request(app)
+      .get(
+        '/finance-and-id/confirm-add-an-id/?countryBornIn=&costOfApplication=-100&applicationSubmittedYear=2000&applicationSubmittedMonth=12&applicationSubmittedDay=12&idType=Birth+certificate&prisonerNumber=A8731DY&existingIdTypes=',
+      )
+      .expect(200)
+      .expect(res => expect(res.text).toMatchSnapshot())
+  })
+  it('Confirm add an ID - birth certificate submit with cost above zero and no answers', async () => {
+    await request(app)
+      .get(
+        '/finance-and-id/confirm-add-an-id/?countryBornIn=&costOfApplication=100&applicationSubmittedYear=2000&applicationSubmittedMonth=12&applicationSubmittedDay=12&idType=Birth+certificate&prisonerNumber=A8731DY&existingIdTypes=',
+      )
+      .expect(200)
+      .expect(res => expect(res.text).toMatchSnapshot())
+  })
+  it('Confirm add an ID - birth certificate submit with cost above zero and no gro number', async () => {
+    await request(app)
+      .get(
+        '/finance-and-id/confirm-add-an-id/?haveGro=false&countryBornIn=&costOfApplication=100&applicationSubmittedYear=2000&applicationSubmittedMonth=12&applicationSubmittedDay=12&idType=Birth+certificate&prisonerNumber=A8731DY&existingIdTypes=',
+      )
+      .expect(200)
+      .expect(res => expect(res.text).toMatchSnapshot())
+  })
+  it('Confirm add an ID - birth certificate submit with cost above zero and gro number', async () => {
+    await request(app)
+      .get(
+        '/finance-and-id/confirm-add-an-id/?haveGro=true&countryBornIn=&costOfApplication=100&applicationSubmittedYear=2000&applicationSubmittedMonth=12&applicationSubmittedDay=12&idType=Birth+certificate&prisonerNumber=A8731DY&existingIdTypes=',
       )
       .expect(200)
       .expect(res => expect(res.text).toMatchSnapshot())
