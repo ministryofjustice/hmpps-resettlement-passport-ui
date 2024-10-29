4c0eb3008c900744f55325046bb9cebe0b608d08
abdulrkalam <abdul.rahuman@digital.justice.gov.uk>
2024-10-29 15:38:10 +0000

    PSFR-1798 Add Controller unit test for staff dashboard

--- server/routes/testutils/testUtils.ts
+++ server/routes/testutils/testUtils.ts
@@ -1,5 +1,7 @@
 import { PrisonerData } from '../../@types/express'
+
 import RpService from '../../services/rpService'
+import { PrisonersList } from '../../data/model/prisoners'
 
 export function stubPrisonerDetails(rpService: RpService, releaseDate: string = null) {
   jest.spyOn(rpService, 'getPrisonerDetails').mockResolvedValue({
@@ -17,6 +19,72 @@ export function stubPrisonerDetails(rpService: RpService, releaseDate: string = 
       },
     ],
   } as PrisonerData)
+}
+
+export function stubPrisonersList(rpService: RpService) {
+  const prisonerList: PrisonersList = {
+    content: [
+      {
+        prisonerNumber: 'G9808UX',
+        firstName: 'CASEY',
+        middleNames: 'AMARAWN',
+        lastName: 'CHAVAN',
+        releaseDate: '2024-11-30',
+        releaseType: 'CRD',
+        lastUpdatedDate: null,
+        status: [
+          {
+            pathway: 'ACCOMMODATION',
+            status: 'NOT_STARTED',
+            lastDateChange: null,
+          },
+          {
+            pathway: 'ATTITUDES_THINKING_AND_BEHAVIOUR',
+            status: 'NOT_STARTED',
+            lastDateChange: null,
+          },
+          {
+            pathway: 'CHILDREN_FAMILIES_AND_COMMUNITY',
+            status: 'NOT_STARTED',
+            lastDateChange: null,
+          },
+          {
+            pathway: 'DRUGS_AND_ALCOHOL',
+            status: 'NOT_STARTED',
+            lastDateChange: null,
+          },
+          {
+            pathway: 'EDUCATION_SKILLS_AND_WORK',
+            status: 'NOT_STARTED',
+            lastDateChange: null,
+          },
+          {
+            pathway: 'FINANCE_AND_ID',
+            status: 'NOT_STARTED',
+            lastDateChange: null,
+          },
+          {
+            pathway: 'HEALTH',
+            status: 'NOT_STARTED',
+            lastDateChange: null,
+          },
+        ],
+        pathwayStatus: null,
+        homeDetentionCurfewEligibilityDate: null,
+        paroleEligibilityDate: null,
+        releaseEligibilityDate: null,
+        releaseEligibilityType: null,
+        releaseOnTemporaryLicenceDate: null,
+        assessmentRequired: true,
+      },
+    ],
+    pageSize: 10,
+    page: 0,
+    sortName: 'releaseDate,ASC',
+    totalElements: 92,
+    last: false,
+  }
+  return jest.spyOn(rpService, 'getListOfPrisoners').mockResolvedValue(prisonerList)
 }
 
 export function sanitiseStackTrace(html: string) {
