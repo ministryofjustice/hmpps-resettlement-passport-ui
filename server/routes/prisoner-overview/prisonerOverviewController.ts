ad6d7674b182c3e64ff0126152c7125804b3cfb0
kivi corn <akiva.moshecorn@digital.justice.gov.uk>
2024-10-31 15:26:15 +0000

    fix json

--- server/routes/prisoner-overview/prisonerOverviewController.ts
+++ server/routes/prisoner-overview/prisonerOverviewController.ts
@@ -63,9 +63,6 @@ export default class PrisonerOverviewController {
         req,
       )
 
-      console.log(JSON.stringify(staffContacts, null, 2))
-      console.log(JSON.stringify(appointments, null, 2))
-
       return res.render('pages/overview', {
         licenceConditions,
         prisonerData,
