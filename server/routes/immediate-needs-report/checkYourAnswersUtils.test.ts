1b489962c5a413d013e2438b953dfc231ca163cb
Matthew Kerry <matthew.kerry@digital.justice.gov.uk>
2024-11-07 12:05:12 +0000

    Additional unit test

--- server/routes/immediate-needs-report/checkYourAnswersUtils.test.ts
+++ server/routes/immediate-needs-report/checkYourAnswersUtils.test.ts
@@ -1,72 +1,11 @@
 import { CachedQuestionAndAnswer } from '../../data/model/immediateNeedsReport'
 import { categoriseForCheckYourAnswers } from './checkYourAnswersUtils'
+import accommodationV3Answers from './accommodation-answers-v3.json'
+import accommodationV1Answers from './accommodation-answers-v1.json'
 
 describe('categoriseForCheckYourAnswers', () => {
   it('Should populate support needs and details for v3', () => {
-    const mergedQuestionsAndAnswers: CachedQuestionAndAnswer[] = [
-      {
-        question: 'WHERE_DID_THEY_LIVE',
-        questionTitle: 'Where did the person in prison live before custody?',
-        questionType: 'RADIO',
-        pageId: 'ACCOMMODATION_REPORT',
-        answer: {
-          answer: 'NO_PERMANENT_OR_FIXED',
-          displayText: 'No permanent or fixed address',
-          '@class': 'StringAnswer',
-        },
-      },
-      {
-        question: 'WHERE_DID_THEY_LIVE_ADDITIONAL_DETAILS',
-        questionTitle: 'Additional details',
-        questionType: 'LONG_TEXT',
-        pageId: 'ACCOMMODATION_REPORT',
-        answer: { answer: '', displayText: '', '@class': 'StringAnswer' },
-      },
-      {
-        question: 'WHERE_WILL_THEY_LIVE',
-        questionTitle: 'Where will the person in prison live when they are released?',
-        questionType: 'RADIO',
-        pageId: 'ACCOMMODATION_REPORT',
-        answer: {
-          answer: 'DOES_NOT_HAVE_ANYWHERE',
-          displayText: 'Does not have anywhere to live',
-          '@class': 'StringAnswer',
-        },
-      },
-      {
-        question: 'WHERE_WILL_THEY_LIVE_ADDITIONAL_DETAILS',
-        questionTitle: 'Additional details',
-        questionType: 'LONG_TEXT',
-        pageId: 'ACCOMMODATION_REPORT',
-        answer: { answer: 'Monkey', displayText: 'Monkey', '@class': 'StringAnswer' },
-      },
-      {
-        question: 'SUPPORT_REQUIREMENTS',
-        questionTitle: 'Support needs',
-        questionType: 'CHECKBOX',
-        pageId: 'SUPPORT_REQUIREMENTS',
-        answer: {
-          answer: ['HELP_TO_FIND_ACCOMMODATION', 'HELP_TO_KEEP_HOME'],
-          displayText: ['Help to find accommodation', 'Help to keep their home while in prison'],
-          '@class': 'ListAnswer',
-        },
-      },
-      {
-        question: 'SUPPORT_REQUIREMENTS_ADDITIONAL_DETAILS',
-        questionTitle: 'Additional details',
-        questionType: 'LONG_TEXT',
-        pageId: 'SUPPORT_REQUIREMENTS',
-        answer: { answer: 'FRANK BUTCHER', displayText: 'FRANK BUTCHER', '@class': 'StringAnswer' },
-      },
-      {
-        question: 'SUPPORT_NEEDS',
-        questionTitle: 'Accommodation resettlement status',
-        questionType: 'RADIO',
-        pageId: 'ASSESSMENT_SUMMARY',
-        answer: { answer: 'SUPPORT_REQUIRED', displayText: 'Support required', '@class': 'StringAnswer' },
-      },
-    ]
-
+    const mergedQuestionsAndAnswers = accommodationV3Answers as CachedQuestionAndAnswer[]
     const { restQuestions, supportNeeds, supportNeedsDetails, status, caseNote } =
       categoriseForCheckYourAnswers(mergedQuestionsAndAnswers)
 
@@ -80,6 +19,23 @@ describe('categoriseForCheckYourAnswers', () => {
     expect(supportNeedsDetails?.answer?.answer).toEqual('FRANK BUTCHER')
     expect(status?.answer.answer).toEqual('SUPPORT_REQUIRED')
     expect(caseNote).toBeNull()
+  })
+
+  it('Should populate case note for v1', () => {
+    const mergedQuestionsAndAnswers = accommodationV1Answers as CachedQuestionAndAnswer[]
+    const { restQuestions, supportNeeds, supportNeedsDetails, status, caseNote } =
+      categoriseForCheckYourAnswers(mergedQuestionsAndAnswers)
+
+    expect(questionIdsOf(restQuestions)).toEqual([
+      'WHERE_DID_THEY_LIVE',
+      'WHERE_DID_THEY_LIVE_ADDRESS',
+      'HELP_TO_KEEP_HOME',
+      'WHERE_WILL_THEY_LIVE_1',
+    ])
+    expect(supportNeeds).toBeNull()
+    expect(supportNeedsDetails).toBeNull()
+    expect(status?.answer.answer).toEqual('SUPPORT_NOT_REQUIRED')
+    expect(caseNote?.answer.answer).toEqual('Monkey!')
   })
 })
 
