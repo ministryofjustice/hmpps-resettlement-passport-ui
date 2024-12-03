f6688fb08ea05033c5c95fceccf3122518e99363
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2024-12-03 10:32:34 +0000

    PSFR-1929 Add versioning to service updates page

--- server/routes/service-updates/serviceUpdatesController.test.ts
+++ server/routes/service-updates/serviceUpdatesController.test.ts
@@ -4,16 +4,14 @@ import { appWithAllRoutes } from '../testutils/appSetup'
 import Config from '../../s3Config'
 import FeatureFlags from '../../featureFlag'
 import { configHelper } from '../configHelperTest'
+import { ConfigFile } from '../../@types/express'
 
 let app: Express
 const config: jest.Mocked<Config> = new Config() as jest.Mocked<Config>
 const featureFlags: jest.Mocked<FeatureFlags> = new FeatureFlags() as jest.Mocked<FeatureFlags>
 
 beforeEach(() => {
-  configHelper(config)
-
   app = appWithAllRoutes({})
-
   FeatureFlags.getInstance = jest.fn().mockReturnValue(featureFlags)
 })
 
@@ -23,7 +21,57 @@ afterEach(() => {
 
 describe('ServiceUpdatesController', () => {
   describe('getView', () => {
-    it('should render the service updates page without specific page parameter', async () => {
+    function getConfigWithWhatsNewVersion(version: string): ConfigFile {
+      return {
+        reports: {
+          immediateNeedsVersion: {
+            ACCOMMODATION: 2,
+            ATTITUDES_THINKING_AND_BEHAVIOUR: 1,
+            CHILDREN_FAMILIES_AND_COMMUNITY: 1,
+            DRUGS_AND_ALCOHOL: 1,
+            EDUCATION_SKILLS_AND_WORK: 1,
+            FINANCE_AND_ID: 1,
+            HEALTH: 1,
+          },
+          preReleaseVersion: {
+            ACCOMMODATION: 2,
+            ATTITUDES_THINKING_AND_BEHAVIOUR: 1,
+            CHILDREN_FAMILIES_AND_COMMUNITY: 1,
+            DRUGS_AND_ALCOHOL: 1,
+            EDUCATION_SKILLS_AND_WORK: 1,
+            FINANCE_AND_ID: 1,
+            HEALTH: 1,
+          },
+        },
+        whatsNew: {
+          enabled: true,
+          version,
+        },
+      }
+    }
+
+    it('should render the service updates page without specific page parameter - future version number (i.e. show everything)', async () => {
+      configHelper(config, getConfigWithWhatsNewVersion('99991231'))
+      await request(app)
+        .get('/service-updates')
+        .expect(200)
+        .expect(res => {
+          expect(res.text).toMatchSnapshot()
+        })
+    })
+
+    it('should render the service updates page without specific page parameter - specific version number', async () => {
+      configHelper(config, getConfigWithWhatsNewVersion('20241120'))
+      await request(app)
+        .get('/service-updates')
+        .expect(200)
+        .expect(res => {
+          expect(res.text).toMatchSnapshot()
+        })
+    })
+
+    it('should render the service updates page without specific page parameter - past version number', async () => {
+      configHelper(config, getConfigWithWhatsNewVersion('20240101'))
       await request(app)
         .get('/service-updates')
         .expect(200)
@@ -33,6 +81,7 @@ describe('ServiceUpdatesController', () => {
     })
 
     it('should render the service updates subpage when a page parameter is provided', async () => {
+      configHelper(config)
       await request(app)
         .get('/service-updates/inputting-report-answers')
         .expect(200)
@@ -41,8 +90,9 @@ describe('ServiceUpdatesController', () => {
         })
     })
 
-    it('should return 404 if the specified template does not exist (error path)', async () => {
-      await request(app).get('/service-updates/invalid-page').expect(404)
+    it('should return 500 if the specified template does not exist (error path)', async () => {
+      configHelper(config)
+      await request(app).get('/service-updates/invalid-page').expect(500)
     })
   })
 })
