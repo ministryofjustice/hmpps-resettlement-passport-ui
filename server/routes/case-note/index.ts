8281e4654e92d3734bfd79cba3c3be0c6d9519af
abdulrkalam <129852408+abdulrkalam@users.noreply.github.com>
2024-12-03 14:20:28 +0000

    PSFR-747 - Refactor add case note page to use MVC (#489)
    * PSFR-747 - Refactor add case note page to use MVC
    
    * PSFR-747 - Refactor add case note page to use MVC
    
    * PSFR-747 - Refactor add case note page to use MVC - review fix

---
+++ server/routes/case-note/index.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+import { Services } from '../../services'
+import CaseNoteController from './caseNoteController'
+
+export default (router: Router, services: Services) => {
+  const caseNoteController = new CaseNoteController(services.prisonerDetailsService)
+
+  router.get('/add-case-note', [caseNoteController.getView])
+}
