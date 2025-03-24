68ee0b2b71b525ece875a57a3a68fd3e5d1d0c3a
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2025-03-24 16:22:51 +0000

    fix comments

--- server/middleware/setUpCsrf.ts
+++ server/middleware/setUpCsrf.ts
@@ -11,7 +11,8 @@ export default function setUpCsrf(): Router {
     const {
       csrfSynchronisedProtection, // This is the default CSRF protection middleware.
     } = csrfSync({
-      // By default, csrf-sync uses x-csrf-token header, but we use the token in forms and send it in the request body, so change getTokenFromRequest so it grabs from there
+      // By default, csrf-sync uses x-csrf-token header, but we also use the token in forms and send it in the request body or as a query parameter,
+      // so change getTokenFromRequest so it grabs from there
       getTokenFromRequest: req => {
         // eslint-disable-next-line no-underscore-dangle
         return req.body._csrf || req.query._csrf || req.headers['x-csrf-token']
