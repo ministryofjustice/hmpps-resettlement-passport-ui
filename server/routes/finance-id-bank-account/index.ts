7ac5429d4ea2a5e55f91c6fe25573388e50c2af6
Mary Ford <201111422+Mary-ford@users.noreply.github.com>
2025-06-06 10:06:32 +0100

    WIP

--- server/routes/finance-id-bank-account/index.ts
+++ server/routes/finance-id-bank-account/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { Services } from '../../services'
 import FinanceIdBankAccountController from './financeIdBankAccountController'
 import asyncWrapper from '../asyncWrapper'
+import { readOnlyGuard } from '../readOnlyGuard'
 
 export default (router: Router, services: Services) => {
   const financeIdBankAccountController = new FinanceIdBankAccountController(
@@ -10,18 +11,23 @@ export default (router: Router, services: Services) => {
   )
 
   router.post('/finance-and-id/bank-account-submit', [
+    readOnlyGuard,
     asyncWrapper(financeIdBankAccountController.postBankAccountSubmitView),
   ])
   router.post('/finance-and-id/bank-account-update', [
+    readOnlyGuard,
     asyncWrapper(financeIdBankAccountController.postBankAccountUpdateView),
   ])
   router.get('/finance-and-id/add-a-bank-account', [
+    readOnlyGuard,
     asyncWrapper(financeIdBankAccountController.getAddABankAccountView),
   ])
   router.get('/finance-and-id/update-bank-account-status', [
+    readOnlyGuard,
     asyncWrapper(financeIdBankAccountController.getUpdateBankAccountStatusView),
   ])
   router.get('/finance-and-id/confirm-add-a-bank-account', [
+    readOnlyGuard,
     asyncWrapper(financeIdBankAccountController.getConfirmAddABankAccountView),
   ])
 }
